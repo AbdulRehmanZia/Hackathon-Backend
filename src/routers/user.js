ec380ff1e6e74fa8c417b46eb225a5632fe03c9e
AbdulRehmanZia <abdulrehmanzia2021@gmail.com>
2025-01-25 10:37:41 +0500

    done with get single user api

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,72 +1,89 @@
 import userModel from "../models/userModel.js";
-import {
-  editUserSchema,
-} from "../validations/schemas.js";
+import { editUserSchema } from "../validations/schemas.js";
 import sendResponse from "../utilis/sendResponse.js";
 import express from "express";
 import { authenticateUser } from "../middleware/authenticateUser.js";
 
 const router = express.Router();
 
-//edit user
-
-
-router.get('/all-users', async (req, res) => {
+//get all users
+router.get("/all-users", async (req, res) => {
   try {
     const users = await userModel.find();
     sendResponse(res, 200, users, true, "Users Fetched Successfully");
-  }
-  catch (err) {
+  } catch (err) {
     sendResponse(res, 500, null, false, "Something went wrong");
   }
-}
-)
+});
+
+//get single user
+
+router.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await userModel.findById(id);
+    if (!user) {
+      return sendResponse(res, 404, null, false, "User not found.");
+    }
+    return sendResponse(res, 200, user, true, "User fetched successfully.");
+  } catch (error) {
+    console.error(error);
+    return sendResponse(res, 500, null, false, "Internal Server Error.");
+  }
+});
+
+//edit user
 router.put("/edit/:id", authenticateUser, async (req, res) => {
   console.log("Editing user...");
-try {
+  try {
     const { id } = req.params;
     const { value, error } = editUserSchema.validate(req.body);
-  
+
     if (error) return sendResponse(res, 400, null, false, error.message);
-  
+
     const userExists = userModel.findById(id);
-  
+
     if (!userExists)
       return sendResponse(res, 404, null, false, "User not Found.");
-  
+
     const updatedUserObj = { ...value };
-  
+
     const updatedUser = await userModel.findByIdAndUpdate(
       id,
       { $set: { ...updatedUserObj } },
       { new: true, runValidators: true }
     );
-  
+
     if (!updatedUser) {
       return sendResponse(res, 404, null, false, "Failed to edit user.");
     }
-  
-    return sendResponse(res, 201, updatedUser, true, "User edited successfully.");
-  
-} catch (error) {
+
+    return sendResponse(
+      res,
+      201,
+      updatedUser,
+      true,
+      "User edited successfully."
+    );
+  } catch (error) {
     console.error(error);
     return sendResponse(res, 500, null, false, "Internal Server Error.");
-}
+  }
 });
 
 //delete user
 
 router.delete("/delete/:id", async (req, res) => {
- try {
+  try {
     const { id } = req.params;
 
     const userExists = await userModel.findById(id);
-  
+
     if (!userExists)
       return sendResponse(res, 404, null, false, "User not found.");
-  
+
     const deletedUser = await userModel.findByIdAndDelete(id);
-  
+
     return sendResponse(
       res,
       201,
@@ -74,10 +91,10 @@ router.delete("/delete/:id", async (req, res) => {
       true,
       "User deleted successfully."
     );
- } catch (error) {
+  } catch (error) {
     console.error(error);
     return sendResponse(res, 500, null, false, "Internal Server Error.");
- }
+  }
 });
 
 export default router;
